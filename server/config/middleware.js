b137fda680ff3159d1606aa5b4cff3c48b9629a5
Danilo Jr <danilobjr@gmail.com>
2014-04-08 19:05:23 -0300

    authorization middleware

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,6 +1,13 @@
 'use strict';
 
 module.exports = {
+	isLoggedIn: function (req, res, next) {
+		if (req.isAuthenticated()) { 
+			next(); 
+		} else {
+			res.redirect('/login');
+		}
+	},
 	putUserInResLocal: function (req, res, next) {
 		res.locals.session = undefined;
 
