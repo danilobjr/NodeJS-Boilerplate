26f8dc34d167008cfb8b5be4a9f89c2d5683e443
Danilo Jr <danilobjr@gmail.com>
2014-04-08 13:55:19 -0300

    server login code

--- server/web/controllers/accountController.js
+++ server/web/controllers/accountController.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var passport = require('passport');
+
 module.exports.login = function (req, res) {
 	res.render('account/login');
 };
+
+module.exports.signin = passport.authenticate('local', { 
+	successRedirect: '/',
+	failureRedirect: '/login',
+	failureFlash: true
+});
--- server/web/routes/accountRoutes.js
+++ server/web/routes/accountRoutes.js
@@ -4,4 +4,5 @@ var accountController = require('./../controllers/accountController');
 
 module.exports = function (app) {
 	app.get('/login', accountController.login);
+	app.post('/login', accountController.signin);
 };
