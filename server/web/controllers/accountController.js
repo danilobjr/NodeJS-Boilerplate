f997739d619ff8978a3c9feae57b91931472f784
Danilo Jr <danilobjr@gmail.com>
2014-04-11 00:30:16 -0300

    signup server side code

--- server/web/controllers/accountController.js
+++ server/web/controllers/accountController.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var passport = require('passport');
+var passport = require('passport'),
+	mongoose = require('mongoose'),
+	User = mongoose.model('User');
 
 module.exports.loginPage = function (req, res) {
 	res.render('account/login', { message: req.flash('login') });
@@ -26,10 +28,32 @@ module.exports.signupPage = function (req, res) {
 	});
 };
 
-module.exports.signup = function (req, res) {
+module.exports.signup = function (req, res, next) {
 	console.log(req.body);
 
-	req.flash('signup-success', true);
-	req.flash('signup-message', 'Go to sign in page');
-	res.redirect('/signup');
+	var newUser = new User({
+		email: req.body.email
+	});
+
+	newUser.fullName = req.body.fullName;
+	newUser.password = req.body.password;
+
+	newUser.save(function (error, user, numberAffected) {
+		if (error) { return next(error); }
+
+		var success = false,
+			message = '';
+
+		if (numberAffected) {
+			success = true;
+			message = 'Go to sign in page';
+		} else {
+			message = 'User not saved';
+		}
+
+		req.flash('signup-success', success);
+		req.flash('signup-message', message);
+		res.redirect('/signup');
+	});
+
 };
