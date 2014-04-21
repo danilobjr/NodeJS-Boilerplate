51107dc8f542991739713dd222b28e85dca1cec4
Danilo Jr <danilobjr@gmail.com>
2014-04-21 11:58:34 -0300

    running mocha tests on grunt default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,8 @@
 module.exports = function (grunt) {
 	// define configuration to all tasks
 	grunt.initConfig({
+
+		// show errors in js files
 		jshint: {
 			options: {
 				jshintrc: true,
@@ -10,13 +12,29 @@ module.exports = function (grunt) {
 			},
 			server: ['server/**/*.js']
 		},
+
+		// run mocha tests
+		mochaTest: {
+			options: {
+				require: 'should',
+				recursive: true,
+				//reporter: 'spec',
+				bail: true,
+			},
+			src: ['test/**/*.js']
+		}
 	});
 
 	// load grunt plugins
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-mocha-test');
 
+	// run 'grunt serve' on console to open app in browser
 	grunt.registerTask('serve', function () {
-		grunt.task.run(['jshint:server']);
+		grunt.task.run([
+			'jshint:server',
+			'mochaTest'
+		]);
 	});
 
 	// define tasks
