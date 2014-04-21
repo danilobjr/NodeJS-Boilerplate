1d2e85ca0751428bb1e8819a676126276a72aeed
Danilo Jr <danilobjr@gmail.com>
2014-04-21 10:20:01 -0300

    basic grunt configuration

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function (grunt) {
+	// define configuration to all tasks
+	grunt.initConfig({});
+
+	// load grunt plugins
+	grunt.loadNpmTasks();
+
+	// define tasks
+	grunt.registerTask('default');
+};
